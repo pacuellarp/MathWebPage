16af6ff239306ee41f499b95f855339c56561364
Paul Cuéllar <pacuellarp@unal.edu.co>
2022-12-31 20:28:16 -0500

    Interest rate conversor with a few bug to fix

--- pages/wage/wage.js
+++ pages/wage/wage.js
@@ -1,22 +1,17 @@
 let interestMode = document.getElementById('interestMode');
 let interestData = document.getElementById('interestData');
-let nRate = document.getElementById('nRate');
-let nRateTime = document.getElementById('nRateTime');
-let  = document.getElementById('');
-
-
 
 class WriteInterest{
     constructor(){
         this.timeRate=`
-            <option value="Annual">Annual</option>
-            <option value="Biannual">Biannual</option>
-            <option value="Four-monthly">Four-monthly</option>
-            <option value="Quarterly">Quarterly</option>
-            <option value="Bimonthly">Bimonthly</option>
-            <option value="Monthly">Monthly</option>
-            <option value="Biweekly">Biweekly</option>
-            <option value="Diary">Diary</option>
+            <option value="1">Annual</option>
+            <option value="2">Biannual</option>
+            <option value="3">Four-monthly</option>
+            <option value="4">Quarterly</option>
+            <option value="6">Bimonthly</option>
+            <option value="12">Monthly</option>
+            <option value="24">Biweekly</option>
+            <option value="360">Diary</option>
         </select>`;
         this.NTE=`<p>Nominal Interest Rate (%):</p>
         <div>
@@ -28,7 +23,7 @@ class WriteInterest{
         <p>Compounding Frequency:</p>
         <select id="nRateCompTime">${this.timeRate}
         <div class="buttonContainer">
-            <button type="button" onclick="calInterest">Calculate</button>
+            <button type="button" onclick="calInterest()">Calculate</button>
             <button type="button" id="clear1">Clear</button>
         </div>
             <p>Resulting Effective Interest Rate:</p>
@@ -36,7 +31,7 @@ class WriteInterest{
             <label for="eRate2">
                 <input id="eRate2" type="number">
             </label>
-            <select id='eRateTime2'>${this.timeRate}
+            <select id='eRate2Time'>${this.timeRate}
         <div/>`;
         this.ETN=`<p>Effective Interest Rate (%):</p>
         <div>
@@ -58,7 +53,7 @@ class WriteInterest{
             </label>
             <select id='nRate2Time'>${this.timeRate}
             </div>`;
-            this.ETE=`<p>Effective Interest Rate (%):</p>
+        this.ETE=`<p>Effective Interest Rate (%):</p>
             <div>
                 <label for="eRate">
                     <input id="eRate" type="number">
@@ -98,29 +93,75 @@ class WriteInterest{
             </label>
             <select id='nRate2Time'>${this.timeRate}
         </div>`;
-        this.eRate={
-        x:'Hola',
-        y:'Adiós',
-        }
-        this.nRate={
-
-        }
+        this.eRate=0;
+        this.nRate=0;
         this.eRate2={
-        x:'Hola',
-        y:'Adiós',
+            annual:0,
+            biannual:0,
+            fourmonthly:0,
+            quarterly:0,
+            bimonthly:0,
+            monthly:0,
+            biweekly:0,
+            diary:0,
         }
         this.nRate2={
-
+            annual:0,
+            biannual:0,
+            fourmonthly:0,
+            quarterly:0,
+            bimonthly:0,
+            monthly:0,
+            biweekly:0,
+            diary:0,
         }
         this.eRateTime;
         this.nRateTime;
         this.nRateCompTime;
         this.nRate2CompTime;
-        
+        this.times=[1,2,3,4,6,12,24,360]
     }
+    ETE(eRate,eRateTime,eRate2Time){
+        return (((1+eRate)^(eRateTime/eRate2Time))-1);
+    }
+    NTN(nRate,nRateTime,nRateCompTime,nRate2CompTime,nRate2Time){
+        return ((((1+(nRate*nRateTime/nRateCompTime))^(nRateCompTime/nRate2CompTime)-1)*nRate2CompTime)/nRate2Time);
+    }
+    calNTE(){
+        this.eRate2.annual=(1+(this.nRate*this.nRateTime/this.nRateCompTime))^(this.nRateCompTime)-1;
+        let i=0;
+        for(const prop in this.eRate2){
+            this.eRate2[prop]=ETE(this.eRate2.annual,1,this.times[i])
+            i+=1;
+        }
+    }
+    calETN(){
+        this.eRate=this.ETE(this.eRate,this.eRateTime,1);
+        this.nRate2.annual=((this.eRate+1)^(1/this.this.nRate2CompTime)-1)*this.nRate2CompTime;
+        let i=0;
+        for(const prop in this.nRate2){
+            this.nRate2[prop]=this.NTN(this.nRate2.annual,1,this.nRate2CompTime,this.nRate2CompTime,this.times[i]);
+            i+=1;
+        }
+    }
+    calETE(){
+        let i=0;
+        for(const prop in this.eRate2){
+            this.eRate2[prop]=this.ETE(this.eRate,this.eRateTime,this.times[i]);
+            i+=1;
+        }
+    }
+    calNTN(){
+        let i=0;
+        for(const prop in this.nRate2){
+            this.nRate2[prop]=this.NTN(this.nRate,this.nRateTime,this.nRateCompTime,this.nRate2CompTime,this.times[i]);
+            i+=1;
+        }
+    };
 }
 
 let wi= new WriteInterest()
+
 
 interestMode.addEventListener('change',()=>{
     if(interestMode.value=="NomToEff"){
@@ -133,4 +174,30 @@ interestMode.addEventListener('change',()=>{
         interestData.innerHTML=wi.NTN;
     }
 },interestData.innerHTML=wi.NTE)
+
+let nRate = document.getElementById('nRate');
+let nRate2 = document.getElementById('nRate2');
+let nRateTime = document.getElementById('nRateTime');
+let nRate2Time = document.getElementById('nRate2Time');
+let eRate = document.getElementById('eRate');
+let eRate2 = document.getElementById('eRate2');
+let eRateTime = document.getElementById('eRateTime');
+let eRate2Time = document.getElementById('eRate2Time');
+let nRateCompTime = document.getElementById('nRateCompTime');
+let nRate2CompTime = document.getElementById('nRate2CompTime');
+
+function calInterest(){
+    if(true){
+        wi.eRate=(eRate.value*1)/100;
+        wi.nRate=(nRate.value*1)/100;
+        wi.eRateTime=eRateTime.value*1;
+        wi.nRateTime=nRateTime.value*1;
+        wi.nRateCompTime=nRateCompTime.value*1;
+        wi.nRate2CompTime=nRate2CompTime.value*1;
+
+        wi.calNTE();
+
+    };
+
+};
 
